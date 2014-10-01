340c6c5e5b60a63c7271b4c18d30c1b4c58de3fa
Quadarius <qkmoore@crimson.ua.edu>
2014-10-01 00:17:14 -0500

    more changes
    removed extra assets folder
    removed bootswatch bootstrap
    theme tweaks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+var originalTitle = "Panthera";
+    var titleMessage = ["Come Back Soon!", 
+                        "I Miss You", 
+                        "Panthers Rule!",  
+                        "Is That Nick Saban?", 
+                        "Not Gmail", 
+                        "Mypanther", 
+                        "Conner Lines", 
+                        "Is That A Panther?", 
+                        "Panthera!!", 
+                        "Pantherb",
+                        "Hello Dr. Gray"
+                        ];
 
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
